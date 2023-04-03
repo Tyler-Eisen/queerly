9678a74a73f5f3b130f2294f5fab141943c7a58c
Tyler Eisen <jteisen90@gmail.com>
2023-04-03 11:45:47 +0700

    can now filter events by date

--- pages/event/event.js
+++ pages/event/event.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Col, Row } from 'react-bootstrap';
+import {
+  Button, Col, Row, Form,
+} from 'react-bootstrap';
 import Link from 'next/link';
 import Head from 'next/head';
 import { getEvents } from '../../api/eventData';
@@ -8,17 +10,50 @@ import EventCard from '../../components/EventCard';
 
 export default function EventPage() {
   const [events, setEvents] = useState([]);
+  const [selectedRange, setSelectedRange] = useState('');
 
   const { user } = useAuth();
 
   const getAllTheEvents = () => {
-    getEvents(user.uid).then(setEvents);
+    const startDate = new Date();
+    const endDate = new Date();
+    switch (selectedRange) {
+      case 'next-month':
+        endDate.setMonth(startDate.getMonth() + 1);
+        break;
+      case 'next-three-months':
+        endDate.setMonth(startDate.getMonth() + 3);
+        break;
+      case 'next-six-months':
+        endDate.setMonth(startDate.getMonth() + 6);
+        break;
+      case 'next-year':
+        endDate.setFullYear(startDate.getFullYear() + 1);
+        break;
+      default:
+        // If no range is selected, fetch all events
+        endDate.setFullYear(startDate.getFullYear() + 1000);
+        break;
+    }
+    getEvents().then((data) => {
+      const filteredData = data.filter((event) => {
+        const eventDate = new Date(event.date);
+        return eventDate >= startDate && eventDate < endDate;
+      });
+      // console.warn(filteredData);
+      setEvents(filteredData);
+    });
   };
+  console.warn(selectedRange);
 
   useEffect(() => {
     getAllTheEvents();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedRange]);
+
+  const handleRangeChange = (event) => {
+    setSelectedRange(event.target.value);
+  };
 
   return (
     <>
@@ -29,11 +64,18 @@ export default function EventPage() {
         <div className="summary-container col-md-4 p-4 width: '100%', margin: '0'" style={{ height: '100%', overflowY: 'auto' }}>
           <h2 style={{ fontWeight: 'bold' }}>queerly Events</h2>
           <p style={{ lineHeight: '1.5' }}>Our mission is to help LGBTQ+ individuals find a way to create community in place. Part of that is helping you find events near you which will give you the chance to meet other members of the community while celebrating your queerness! We strive to find a wide variety of events to connect you with and encourage you to add any events you would like others to know about. Click on an event for further information as well as a chance to see what other users are saying about it!</p>
-          <div className="text-center my-4">
+          <div className="">
             <Link href="/event/new" passHref>
               <Button>Add An Event</Button>
             </Link>
           </div>
+          <Form.Select onChange={handleRangeChange} value={selectedRange} style={{ display: 'inline-block', width: 'auto' }}>
+            <option value="">All events</option>
+            <option value="next-month">Next month</option>
+            <option value="next-three-months">Next 3 months</option>
+            <option value="next-six-months">Next 6 months</option>
+            <option value="next-year">Next year</option>
+          </Form.Select>
         </div>
         <div className="card-container col-md-8 p-4" style={{ fontSize: '1rem', height: '100%', overflowY: 'auto' }}>
           <Row className="justify-content-center">
